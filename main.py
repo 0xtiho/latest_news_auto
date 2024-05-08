facec7d9ff61aca1c14403ad6cd459ec1def6882
GovDz <f_boudellal@estin.dz>
2024-05-08 15:32:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+NEWS_API_ENDPOINT = "https://newsapi.org/v2/top-headlines"
+API_KEY = "2f1b877218e84eeaa5c47eef5680e77c"
+
+parameters = {
+    "country": "us",
+    "apiKey": API_KEY
+}
+
+response = requests.get(NEWS_API_ENDPOINT, params=parameters)
+
+if response.status_code == 200:
+    articles = response.json()["articles"]
+    news_data = {"news": []}
+
+    for article in articles:
+        news_data["news"].append({
+            "title": article['title'],
+            "description": article['description'],
+            "url": article['url']
+        })
+
+    with open("latest_news.json", "w") as json_file:
+        json.dump(news_data, json_file, indent=4)
+        
+    print("Latest news has been written to latest_news.json.")
+else:
+    print("Failed to fetch news. Status code:", response.status_code)
+
